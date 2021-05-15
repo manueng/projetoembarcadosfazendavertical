35d08f4c3942c1d679df6e556792ca297bf8d285
Emanuel <manusenac@hotmail.com>
2021-05-14 23:46:19 -0300

    upload code form comunicate the raspberry with arduino.

---
+++ lib_serial-1.c
@@ -0,0 +1,273 @@
+#include "lib_serial.h"
+/*********************************
+ jpegtest.c
+ $ gcc jpegtest.c -std=c11 -ljpeg
+ *********************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <jpeglib.h>
+
+#define   azul 0 
+#define   verde 1 
+#define  vermelho 2 
+// JPEG image struct
+typedef struct {
+    uint8_t *data;   // raw data
+    uint32_t width;
+    uint32_t height;
+    uint32_t ch;     // color channels
+} JpegData;
+ typedef struct {
+    uint8_t azul2;
+    uint8_t verde2;
+    uint8_t  vermelho2;
+ }Cor;  
+// allocate memory for raw data
+void alloc_jpeg(JpegData *jpegData)
+{
+    jpegData->data = NULL;
+    jpegData->data = (uint8_t*) malloc(sizeof(uint8_t)  *
+                                       jpegData->width  *
+                                       jpegData->height *
+                                       jpegData->ch);
+}
+
+// free memory for raw data
+void free_jpeg(JpegData *jpegData)
+{
+    if (jpegData->data != NULL) {
+        free(jpegData->data);
+        jpegData->data = NULL;
+    }
+}
+
+// read JPEG image
+// 1. create JPEG decompression object
+// 2. specify source data
+// 3. read JPEG header
+// 4. set parameters
+// 5. start decompression
+// 6. scan lines
+// 7. finish decompression
+bool read_jpeg(JpegData *jpegData,
+              const char *srcfile,
+              struct jpeg_error_mgr *jerr)
+{
+    // 1.
+    struct jpeg_decompress_struct cinfo;
+    jpeg_create_decompress(&cinfo);
+    cinfo.err = jpeg_std_error(jerr);
+
+    FILE *fp = fopen(srcfile, "rb");
+    if (fp == NULL) {
+        printf("Error: failed to open %s\n", srcfile);
+        return false;
+    }
+    // 2.
+    jpeg_stdio_src(&cinfo, fp);
+
+    // 3.
+    jpeg_read_header(&cinfo, TRUE);
+
+    // 4. omitted
+    // 5.
+    jpeg_start_decompress(&cinfo);
+
+    jpegData->width  = cinfo.image_width;
+    jpegData->height = cinfo.image_height;
+    jpegData->ch     = cinfo.num_components;
+
+    alloc_jpeg(jpegData);
+
+    // 6. read line by line
+    uint8_t *row = jpegData->data;
+    const uint32_t stride = jpegData->width * jpegData->ch;
+    for (int y = 0; y < jpegData->height; y++) {
+        jpeg_read_scanlines(&cinfo, &row, 1);
+        row += stride;
+    }
+
+    // 7.
+    jpeg_finish_decompress(&cinfo);
+    jpeg_destroy_decompress(&cinfo);
+    fclose(fp);
+
+    return true;
+}
+
+// write JPEG image
+// 1. create JPEG compression object
+// 2. specify destination data
+// 3. set parameters
+// 4. start compression
+// 5. scan lines
+// 6. finish compression
+bool write_jpeg(const JpegData *jpegData,
+                const char *dstfile,
+                struct jpeg_error_mgr *jerr)
+{
+    // 1.
+    struct jpeg_compress_struct cinfo;
+    jpeg_create_compress(&cinfo);
+    cinfo.err = jpeg_std_error(jerr);
+
+    FILE *fp = fopen(dstfile, "wb");
+    if (fp == NULL) {
+        printf("Error: failed to open %s\n", dstfile);
+        return false;
+    }
+    // 2.
+    jpeg_stdio_dest(&cinfo, fp);
+
+    // 3.
+    cinfo.image_width      = jpegData->width;
+    cinfo.image_height     = jpegData->height;
+    cinfo.input_components = jpegData->ch;
+    cinfo.in_color_space   = JCS_RGB;
+    jpeg_set_defaults(&cinfo);
+
+    // 4.
+    jpeg_start_compress(&cinfo, TRUE);
+
+    // 5.
+    uint8_t *row = jpegData->data;
+    const uint32_t stride = jpegData->width * jpegData->ch;
+    for (int y = 0; y < jpegData->height; y++) {
+        jpeg_write_scanlines(&cinfo, &row, 1);
+        row += stride;
+    }
+
+    // 6.
+    jpeg_finish_compress(&cinfo);
+    jpeg_destroy_compress(&cinfo);
+    fclose(fp);
+
+    return true;
+}
+
+int colore(){
+ JpegData jpegData;
+    struct jpeg_error_mgr jerr;
+
+    // src/dst file
+    char *src = "./alfaceeditada.jpg";
+    char *dst = "./out.jpg";
+
+    if (!read_jpeg(&jpegData, src, &jerr)){
+        free_jpeg(&jpegData);
+        return -1;
+    }
+    printf("Read:  %s\n", src);
+
+    int size = jpegData.width * jpegData.height * jpegData.ch;
+    int contacor;
+    int contaverde;   
+      contacor=0;
+      contaverde=0;
+    int contaverdadeiro=0;
+    uint8_t*   data=jpegData.data;
+    Cor cororiginal;       
+    for (int i = 0; i < size; i++){
+         if(contacor==azul){
+          cororiginal.azul2=jpegData.data[i];
+          contacor++;
+         }else if(contacor==verde){
+           cororiginal.verde2=jpegData.data[i];
+           contacor++;      
+         }else{
+           cororiginal.vermelho2=jpegData.data[i];
+           if((cororiginal.azul2>10&&cororiginal.verde2>55&&cororiginal.verde2<240)||(cororiginal.verde2>55&&cororiginal.vermelho2>10&&cororiginal.verde2<240&&cororiginal.verde2)){    
+              int i2;
+              i=i-3;
+              for(i2=0;i2<3;i2++,i++){
+                jpegData.data[i]=255; 
+              }        
+              contaverde++;
+             }
+           contacor=0;   
+         }  
+        //substituir pela lógica da troca de coresx
+         
+    }
+    printf(" a quantidade de pixels verde e %d", contaverde);  
+    if (!write_jpeg(&jpegData, dst, &jerr)){
+        free_jpeg(&jpegData);
+        return -1;
+    }
+    printf("Write: %s\n", dst);
+
+    free_jpeg(&jpegData);
+
+    return 0;
+}
+
+	int abrir_porta(char *porta_serial)
+{
+    //abrindo a porta serial
+    porta = open(porta_serial, O_RDWR | O_NONBLOCK | O_NDELAY );
+    fcntl(porta, F_SETFL, 0);
+    /*
+    * Armazena os dados das configurações da porta serial
+    */
+    struct termios cfg_porta;
+    if(tcgetattr(porta, &cfg_porta) != 0){
+        printf("Erro ao ler configurações da porta serial: cod. %d msg: %s!\n",errno, strerror(errno));
+        return -1;
+    }
+    
+    cfg_porta.c_cflag &= ~PARENB; // configura para não usar paridade de bits
+    cfg_porta.c_cflag &= ~CSTOPB; // configura um bit de parata
+    cfg_porta.c_cflag &= ~CSIZE; // limpa o tamanho dos dados
+    cfg_porta.c_cflag |= CS8; // configura o tamanho dos bytes em 8 bits
+    cfg_porta.c_cflag &= ~CRTSCTS; // desabilita o controle de fluxo por hardware
+    cfg_porta.c_cflag |= CREAD | CLOCAL; // habilita a leitura
+
+    cfg_porta.c_lflag &= ~ICANON;
+    cfg_porta.c_lflag &= ~ECHO; // desliga o eco por software
+    cfg_porta.c_lflag &= ~ECHOE; // desabilita a interpretação do caractere de backspace
+    cfg_porta.c_lflag &= ~ECHONL; // desabilita o eco pra nova linha
+    cfg_porta.c_lflag &= ~ISIG; // desabilita comandos de suspensão
+    cfg_porta.c_iflag &= ~(IXON | IXOFF | IXANY); // desabilita controle de fluxo de software
+    cfg_porta.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // desabilita tratamentos especias para os bytes recebidos
+
+    cfg_porta.c_oflag &= ~OPOST; // previne a interpretação especial de bytes nas saídas
+    cfg_porta.c_oflag &= ~ONLCR; // previne a conversão de caracteres ASCII especiais
+ 
+    cfg_porta.c_cc[VTIME] = 10;    // configura o tempo de leitura entre os bytes 1 decimo de segundo
+    cfg_porta.c_cc[VMIN] = 0;
+
+    cfsetispeed(&cfg_porta, B9600);// configura a velocidade da recepção para 9600
+    cfsetospeed(&cfg_porta, B9600);// configura a velocidade da transmissão para 9600
+
+    //Aplica as configurações da porta serial
+    if(tcsetattr(porta, TCSANOW, &cfg_porta) != 0){
+        printf("Erro ao salvar configurações da porta serial: cod %d msg: %s \n",errno, strerror(errno));
+        return -2;
+    }
+
+    return 0;
+}
+
+int ler_dados(char *dados)
+{
+    int dados_lidos = read(porta, dados, sizeof(dados));
+    if(dados_lidos < 0){
+        printf("Erro ao ler dados cod: %d msg: %s", errno, strerror(errno));
+        return -1;
+    }
+    return dados_lidos;
+}
+
+int escrever_dados(char *dados)
+{
+    int tamanho_dados = sizeof(dados);
+    int escritos = write(porta, dados, sizeof(dados));
+    if(escritos != tamanho_dados)
+    {
+        printf("Erro Ao escrever dados cod: %d msg %s",errno, strerror(errno));
+        return -1;
+    }
+    return 0;
+}
